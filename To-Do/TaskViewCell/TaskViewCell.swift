b972b27c185daaed72fde659ebb978d27decf49b
Piotr Żakieta <piter5331@gmail.com>
2022-05-23 14:42:52 +0200

    Create Task View Cell with xib file, in xib file add label, imageView and cellIdentifier

---
+++ To-Do/TaskViewCell/TaskViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskViewCell.swift
+//  To-Do
+//
+//  Created by Piotr Żakieta on 23/05/2022.
+//
+
+import UIKit
+
+class TaskViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    
+}
