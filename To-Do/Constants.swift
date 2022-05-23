787be0b281f8c5f09da1ad99b8261c47e3fddd6d
Piotr Żakieta <piter5331@gmail.com>
2022-05-23 14:40:31 +0200

    create Constants file and add two static value taskViewCellNibName, taskViewCellIdentifier

---
+++ To-Do/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  To-Do
+//
+//  Created by Piotr Żakieta on 23/05/2022.
+//
+
+import UIKit
+
+struct K{
+    static let taskViewCellNibName = "TaskViewCell"
+    static let taskViewCellIdentifier = "TaskCell"
+}
