97a3418490d6bfcedeb6d75765a22934664bcbc4
Piotr Żakieta <piter5331@gmail.com>
2022-05-23 19:41:59 +0200

    add new struct to Constants

--- To-Do/Constants.swift
+++ To-Do/Constants.swift
@@ -14,4 +14,12 @@ struct K{
     static let jobIconImage = UIImage(named: "job-icon")
     static let shopIconImage = UIImage(named: "shop-icon")
     static let otherIconImage = UIImage(named: "other-icon")
+    
+    struct EntityTaskNameValue{
+        static let nameEntity = "TasksEntitie"
+        static let typeTask = "typeTask"
+        static let textTask = "textTask"
+        static let isDone = "isDone"
+        static let dateCreateTask = "dateCreateTask"
+    }
 }
