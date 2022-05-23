9a47c7d005cb7d8164f19a13e07cf05fb6164744
Piotr Żakieta <piter5331@gmail.com>
2022-05-23 19:47:33 +0200

    add suport table view

--- To-Do/TasksViewController.swift
+++ To-Do/TasksViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import CoreData
 
 class TasksViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var taskFromCoreData: [TaskData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.register(TaskViewCell.nib(), forCellReuseIdentifier: K.taskViewCellIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+        
+   
+    
+}
+extension TasksViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskFromCoreData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        
+        if let taskCell = tableView.dequeueReusableCell(withIdentifier: K.taskViewCellIdentifier, for: indexPath) as? TaskViewCell{
+            let model = taskFromCoreData[indexPath.row]
+            taskCell.configureTaskCell(idTypeTask: model.typeTask, textTask: model.textTask)
+            
+            cell = taskCell
+        }
+        
+        return cell
     }
 
 
 }
 
+struct TaskData{
+    let typeTask: Int16
+    let textTask: String
+    let isDone: Bool
+    let dateCreateTask: Date
+}
