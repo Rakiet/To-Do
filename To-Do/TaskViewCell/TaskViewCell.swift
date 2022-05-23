59dedfa003173b0a79d96ad2e0034199c194adbc
Piotr Żakieta <piter5331@gmail.com>
2022-05-23 19:48:23 +0200

    add new static func nib and change type form Int to Int16

--- To-Do/TaskViewCell/TaskViewCell.swift
+++ To-Do/TaskViewCell/TaskViewCell.swift
@@ -16,19 +16,23 @@ class TaskViewCell: UITableViewCell {
         // Initialization code
     }
     
-    func configureTaskCell(idTypeTask: Int, textTask: String){
+    func configureTaskCell(idTypeTask: Int16, textTask: String){
         
         imageTypeTask.image = assignIconFormIdToImage(idIcon: idTypeTask)
         textTaskLabel.text = textTask
         
     }
 
-    private func assignIconFormIdToImage(idIcon: Int) -> UIImage{
+    private func assignIconFormIdToImage(idIcon: Int16) -> UIImage{
         switch idIcon{
         case 0: return K.jobIconImage!
         case 1: return K.shopIconImage!
         default: return K.otherIconImage!
         }
+    }
+    
+    static func nib() -> UINib{
+        return UINib(nibName: K.taskViewCellNibName, bundle: nil)
     }
     
 }
