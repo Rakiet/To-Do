a91808c951bc088f9c3d6afd017eb6b71e2569fc
Piotr Żakieta <piter5331@gmail.com>
2022-05-25 10:09:33 +0200

    create TaskModel

---
+++ To-Do/Model/TaskModel.swift
@@ -0,0 +1,15 @@
+//
+//  TaskModel.swift
+//  To-Do
+//
+//  Created by Piotr Żakieta on 23/05/2022.
+//
+
+import Foundation
+
+struct TaskModel{
+    let typeTask: Int16
+    let textTask: String
+    let isDone: Bool
+    let dateCreateTask: Date
+}
