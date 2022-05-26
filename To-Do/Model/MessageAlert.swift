9281275520205f25ea03039070c7916adec406f3
Piotr Żakieta <piter5331@gmail.com>
2022-05-26 11:34:37 +0200

    add new class func showCompletionAlert

--- To-Do/Model/MessageAlert.swift
+++ To-Do/Model/MessageAlert.swift
@@ -17,22 +17,28 @@ class MessageAlert{
         vc.present(alert, animated: true)
     }
     
+    class func showCompletionAlert(title: String, message: String, vc: UIViewController, completionHeander: @escaping  (Bool) -> ()){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction!) in
+            completionHeander(true)
+        }))
+        vc.present(alert, animated: true, completion: nil)
+    }
     
-    
-    class func aceptAlert(title: String, message: String, vc: UIViewController, completionHeander: @escaping  (Bool) -> ()){
-        let refreshAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+    class func deleteAlert(title: String, message: String, vc: UIViewController, completionHeander: @escaping  (Bool) -> ()){
+        let deleteNewAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
 
-        refreshAlert.addAction(UIAlertAction(title: "Potwierdź", style: .destructive, handler: { (action: UIAlertAction!) in
+        deleteNewAlert.addAction(UIAlertAction(title: "Potwierdź", style: .destructive, handler: { (action: UIAlertAction!) in
             completionHeander(true)
         }))
 
-        refreshAlert.addAction(UIAlertAction(title: "Anuluj", style: .default, handler: { (action: UIAlertAction!) in
+        deleteNewAlert.addAction(UIAlertAction(title: "Anuluj", style: .default, handler: { (action: UIAlertAction!) in
             completionHeander(false)
             print("nie usunięto")
         }))
         
         
 
-        vc.present(refreshAlert, animated: true, completion: nil)
+        vc.present(deleteNewAlert, animated: true, completion: nil)
     }
 }
